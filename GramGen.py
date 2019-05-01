009c1ab4f1ae9f8ae099782870f96d36f9f6580c
Mequam <blue9ja@gmail.com>
2019-05-01 08:17:09 -0400

    added a recursive function in the GramGen program to find the absolute path of a given xml tag inside of the document that way we can easily access the parents of the target tags

--- GramGen.py
+++ GramGen.py
@@ -7,13 +7,34 @@ def randStr(string):
     #need a way to specify the file that we want to parse, perhaps we only load the file into memory once?
     #or perhaps we make a generator class?
     #for now we just parse in the given file for quick testing
-    root = ET.parse('gen.xml').getroot()
-    for node in root.findall('.//' + string):
-        if node.text != None:
-            return node.text
-    return 'NULL'
+   
+    return 'POTATO'
+    #tree = ET.parse('gen.xml')
+    #root = tree.getroot()
+    #for node in root.findall('.//' + string):
+    #    if node.text != None:
+    #        return tree.xpath
+    #return 'NULL'
+
+def getAbs(root,tag,path=''):
+    #need to get it to return none if the path that it follows does not contain the tag
+    path += '/' + root.tag
+    if root.tag == tag:
+        return path
+    else:
+        #their tag is not equal, return any of their childrens tags if they are
+        for child in root:
+            lowpath = getAbs(child,tag,path)
+            if lowpath != None:
+                #one of the children found the correct path!, return the path that they give us
+                return lowpath
+        #none of our children or us have the tag, return None
+        return None
+
+tree = ET.parse('gen.xml')
+root = tree.getroot()
+
+print(getAbs(root,'insect'))
+print(root.findall('.//insect')[0].tag)
 print(randStr('testing123 [abc] [insect]'))
-#tree = ET.parse('list.xml')
-#root = tree.getroot()
-#for node in root.findall('.//apples'):
-#    print(node.tag)
+
